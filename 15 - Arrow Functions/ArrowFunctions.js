718047fda5f90533a7a94f118ec51164064b5c5e
Elkin Panameño <elkinsamir643@gmail.com>
2021-11-29 12:17:59 -0500

    Noveno Commit

--- 15 - Arrow Functions/ArrowFunctions.js
+++ 15 - Arrow Functions/ArrowFunctions.js
@@ -1,4 +1,4 @@
-// Podemos declarar funciones de diferentes formas: 
+ // Podemos declarar funciones de diferentes formas: 
 
 // Funciones normales: 
 /* miFuncion();
@@ -11,9 +11,31 @@ function miFuncion(){
 miFuncion(); */
 
 // Funciones Flecha:
-const miFuncionFlecha = () => {
-    console.log('Mi Funcion Flecha');
-}
+// const miFuncionFlecha = () => {
+//     console.log('Mi Funcion Flecha');
+// }
+
+// Podemos simplificar aun mas las funciones flecha: 
+// Esto lo podemos hacer si el cuerpo de nuestra funcion es de una sola linea.
+// const miFuncionFlecha = () => console.log('Mi funcion Flecha');
 
 // La funcion flecha no tiene el mecanismo de Hoisting.
-miFuncionFlecha();
+// miFuncionFlecha();
+
+const saludar = () => 'Hola';
+console.log(saludar());
+
+
+// Podemos ingresar objetos:
+// Pero el cuerpo de la funcion debe ir en parentesis
+const objecto = () => ({nombre: 'Elkin', apellido: 'Angulo'});
+console.log(objecto());
+
+// Funcion Flecha con parametros:
+// Con uno solo podemos quitar los parentesis
+const myArrowFunction = message => {return message};
+console.log(myArrowFunction('Que rico, Sandwich con el pan del Osito :p'));
+
+// Con varios parametros:
+const myArrowFunction2 = (a, b) => `Resultado ${a + b}`;
+console.log(myArrowFunction2(2,3));
